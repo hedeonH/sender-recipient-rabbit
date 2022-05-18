c71b501f9d61560064b2825016e947dbdf9eb314
Patsevko, Olena[External] <Olena.Patsevko@externals.adidas.com>
2022-05-18 11:23:34 +0300

    Initial commit

---
+++ micro-collector/src/main/java/mentoring/microcollector/MicroCollectorApplication.java
@@ -0,0 +1,13 @@
+package mentoring.microcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroCollectorApplication.class, args);
+    }
+
+}
---
+++ micro-recipient/src/test/java/mentoring/microrecipient/MicroRecipientApplicationTests.java
@@ -0,0 +1,13 @@
+package mentoring.microrecipient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRecipientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
