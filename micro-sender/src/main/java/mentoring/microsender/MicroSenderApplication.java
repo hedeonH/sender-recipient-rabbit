6f6facce539d026ab5deb5a295beca76ac0c0255
Patsevko, Olena[External] <Olena.Patsevko@externals.adidas.com>
2022-06-21 12:08:26 +0300

    application config for prometheus and grafana metrics storing

--- micro-sender/src/main/java/mentoring/microsender/MicroSenderApplication.java
+++ micro-sender/src/main/java/mentoring/microsender/MicroSenderApplication.java
@@ -4,10 +4,8 @@ import mentoring.microsender.configuration.RabbitConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties(RabbitConfigurationProperties.class)
 public class MicroSenderApplication {
 
